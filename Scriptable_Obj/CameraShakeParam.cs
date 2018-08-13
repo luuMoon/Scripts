82b459bb6cda54491ac52da3180b3f0158162a3a
luyue <luyue@luckxcyy.com>
2018-08-13 14:33:00 +0800

    添加Scriptable Obj

---
+++ Scriptable_Obj/CameraShakeParam.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "CameraEffect/CameraShakeParam")]
+[System.Serializable]
+public class CameraShakeParam : ScriptableObject
+{
+    [System.Serializable]
+    public class shakeParam
+    {
+        public float amplitudeX;
+        public float amplitudeY;
+        public float duration;
+        public float delayTime;
+    }
+    public shakeParam[] shakeParams;
+}
+
+
